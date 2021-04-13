2936ec2838f21d216b05e532a56ac98fdab53567
= <nexrey19@gmail.com>
2021-04-13 11:04:11 +0700

    add route to articles

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use Illuminate\Http\Request;
 Route::resource('mahasiswa', MahasiswaController::class);
 Route::get('/',[MahasiswaController::class, 'index']);
 Route::get('mahasiswas/nilai/{mahasiswa}', [MahasiswaController::class, 'nilai'])->name('users.nilai');
+Route::resource('articles', ArticleController::class);
 
 
 
