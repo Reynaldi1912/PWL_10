b93eadedb5a3ff227b40e89b38239db5d1e97c27
= <nexrey19@gmail.com>
2021-04-13 15:22:08 +0700

    if migration error , import database to phpmydmin

--- pwl_10.sql
+++ pwl_10.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Apr 2021 pada 13.50
+-- Waktu pembuatan: 13 Apr 2021 pada 10.20
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.4.14
 
@@ -18,8 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pwl_09`
+-- Database: `pwl_10`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `feature_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `content`, `feature_image`, `created_at`, `updated_at`) VALUES
+(1, 'Coba Update', 'Praktikum kedua tentang update', 'images/pwCtYE3bxDhAKfenBh4boNQRXPJJd1FPE4sGrxAb.png', '2021-04-12 21:02:22', '2021-04-12 21:58:35');
 
 -- --------------------------------------------------------
 
@@ -74,6 +96,7 @@ INSERT INTO `kelas` (`id`, `nama_kelas`, `created_at`, `updated_at`) VALUES
 CREATE TABLE `mahasiswa` (
   `nim` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `jurusan` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -84,10 +107,10 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama`, `jurusan`, `created_at`, `updated_at`, `kelas_id`) VALUES
-('1941720070', 'Genadi', 'JTI', NULL, '2021-04-10 03:08:23', 4),
-('1941720142', 'Reynaldi Ramadhani', 'JTI', NULL, NULL, 1),
-('1941720156', 'Riza', 'JTI', '2021-04-12 01:10:48', '2021-04-12 01:10:48', 1);
+INSERT INTO `mahasiswa` (`nim`, `nama`, `image`, `jurusan`, `created_at`, `updated_at`, `kelas_id`) VALUES
+('1941720070', 'Genadi', 'images/q24cbbsuNf83vIZF7BcHWUJyK3iHA54oVx3KBsTs.png', 'JTI', NULL, '2021-04-13 01:00:13', 9),
+('1941720142', 'Reynaldi Ramadhani', 'images/yRLyUsLxpmc9aKfBIX7unIO7OpRh2cEbC6pGSc4c.png', 'JTI', NULL, '2021-04-13 01:00:21', 9),
+('1941720156', 'Riza', 'images/aF312IlqqGEuCyrF2GbKby9bcrU90puxtksYKYrF.png', 'JTI', '2021-04-12 01:10:48', '2021-04-13 01:00:29', 9);
 
 -- --------------------------------------------------------
 
@@ -173,7 +196,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (12, '2021_04_09_122737_create_kelas_table', 1),
 (13, '2021_04_09_124126_relasi_kelas_mahasiswa_table', 2),
 (14, '2021_04_10_015151_drop_kolom', 2),
-(16, '2021_04_10_101428_create_matakuliah_table', 3);
+(16, '2021_04_10_101428_create_matakuliah_table', 3),
+(17, '2021_04_13_012906_create_artices_table', 4),
+(18, '2021_04_13_063309_add_column_foto', 5);
 
 -- --------------------------------------------------------
 
@@ -207,6 +232,12 @@ CREATE TABLE `users` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -266,6 +297,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -293,7 +330,7 @@ ALTER TABLE `matakuliah`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
