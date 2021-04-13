a6acfd39e38e03d1e3a3816f4e6bcf94ce1f6546
= <nexrey19@gmail.com>
2021-04-13 11:03:53 +0700

    make view create

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,20 @@
+@extends('users.layout')
+@section('content')
+<div class="pull-left mt-2">
+ <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
+ </div><br><br>
+<div class="container">
+    <form action="{{url('/articles')}}" method="post" enctype="multipart/form-data">
+    @csrf
+        <div class="form-group">
+        <label for="title">Title: </label>
+            <input type="text" class="form-control" required="required" name="title"></br>
+        <label for="content">Content: </label>
+            <textarea type="text" class="form-control" required="required" name="content"></textarea></br>
+        <label for="image">Feature Image: </label>
+            <input type="file" class="form-control"required="required" name="image"></br>
+                <button type="submit" name="submit" class="btn btn-primary float-right">Simpan</button>
+        </div>
+    </form>
+</div>
+@endsection
